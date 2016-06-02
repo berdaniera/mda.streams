aedee71082ab273d334ac903d500870125d6d50f
Aaron Berdanier <aaron.berdanier@gmail.com>
2016-06-02 12:27:46 -0400

    include view_map

---
+++ R/view_map.R
@@ -0,0 +1,34 @@
+#' Get and browse to a URL to the site location on Google Maps
+#' or plot inline with Leaflet
+#' 
+#' @param site_names a list of site names such as those returned from 
+#'   make_site_name()
+#' @param browser logical. Should the URL be opened in a browser?
+#' @import httr
+#' @import leaflet
+#' @importFrom stats setNames complete.cases
+#' @export
+#' @examples 
+#' \dontrun{
+#' cat(view_google_map("nwis_01484680"))
+#' view_google_map(c("nwis_01467200","nwis_09327000","nwis_351111089512501"))
+#' }
+view_map <- function(site_names, browser=TRUE) {
+  coords <- get_site_coords(site_names, format="normal", attach.units = FALSE)
+  if(browser) { # view with Google Maps
+    url <- setNames(
+      ifelse(complete.cases(coords),
+           paste0("https://www.google.com/maps/place//@", coords$lat, ",", coords$lon,",5z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0"),
+           NA), 
+      site_names)
+    # only open first 10 URLs to avoid creating a bazillion tabs (assuming this would be an accident)
+    if(length(url) > 10) 
+      warning('found >10 URLs and browser=TRUE; only browsing to the first 10')
+    sapply(url[1:min(10, length(url))], function(ur) if(!is.na(ur)) BROWSE(ur))
+    return(url)
+  } else { # view with leaflet
+    m <- leaflet() %>% addTiles() %>%  # Add default OpenStreetMap map tiles
+      addMarkers(lng=coords$lon, lat=coords$lat, popup=coords$site_name)
+    m  # Print the map    
+  }
+}
